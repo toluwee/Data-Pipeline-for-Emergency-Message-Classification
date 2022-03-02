649790e5e1c6e7b81ff387a36ca97e4169bdb930
Tolu Olukoga <onlinetolu@gmail.com>
2022-03-02 11:57:06 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -39,7 +39,6 @@ model = joblib.load("../models/classifier.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
@@ -48,7 +47,6 @@ def index():
     
     
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
             # GRAPH 1 - category graph
         {
